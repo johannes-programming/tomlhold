c414cf7bb8ccb20cc4f056bde741cd151c2317d0
Johannes <johannes-programming@mailfence.com>
2024-10-23 00:49:28 +0200

    u

--- src/tomlhold/core/__init__.py
+++ src/tomlhold/core/__init__.py
@@ -1,13 +1,13 @@
 import datetime
-import functools
 import tomllib
 
 import tomli_w
 
 import datahold
+from typing import *
 
 __all__ = ["Holder"]
-_TYPES = (
+NONITER = (
     bool,
     datetime.date,
     datetime.datetime,
@@ -16,47 +16,55 @@ _TYPES = (
     int,
     str,
 )
+KEYUNION = Union[int, str]
+ITERUNION = Union[dict, list]
+NONITERUNION = Union[NONITER]
+TOTALUNION = Union[NONITERUNION, ITERUNION]
 
 
-def _copy(value):
+def _copy(value:NONITERUNION) -> NONITERUNION:
     if type(value) is dict:
         return _copy_dict(value)
     if type(value) is list:
         return _copy_list(value)
-    if type(value) in _TYPES:
+    if type(value) in NONITER:
         return value
     raise TypeError("type %r is not allowed" % type(value))
 
 
-def _copy_dict(value):
+def _copy_dict(value:Any) -> Dict:
     return {str(k): _copy(value[k]) for k in value.keys()}
 
 
-def _copy_list(value):
+def _copy_list(value:Iterable)->List:
     return [_copy(v) for v in value]
 
 
-def _get_key(k, /):
+def _get_key(k:Any, /)->KEYUNION:
     if issubclass(type(k), str):
         return str(k)
     else:
         return int(k)
 
 
-def _get_keys(keys, /):
+def _get_keys(keys:Iterable, /) -> List[KEYUNION]:
     try:
         return [_get_key(keys)]
     except:
         pass
-    ans = list()
-    for k in keys:
-        ans.append(_get_key(k))
-    return ans
+    return [_get_key(k) for k in keys]
+
+
+def setdocstring(new:Any, /) -> Any:
+    name = new.__name__
+    old = getattr(datahold.OkayDict, name)
+    new.__doc__ = old.__doc__
+    return new
 
 
 class Holder(datahold.OkayDict):
-
-    def __delitem__(self, keys):
+    @setdocstring
+    def __delitem__(self, keys:Iterable) -> None:
         keys = _get_keys(keys)
         if not keys:
             self.clear()
@@ -66,7 +74,8 @@ class Holder(datahold.OkayDict):
             ans = ans[keys.pop(0)]
         del ans[keys[0]]
 
-    def __getitem__(self, keys):
+    @setdocstring
+    def __getitem__(self, keys:Iterable):
         keys = _get_keys(keys)
         ans = self._data
         for k in keys:
@@ -74,13 +83,8 @@ class Holder(datahold.OkayDict):
         ans = _copy(ans)
         return ans
 
-    def __init__(self, string="") -> None:
-        self._data = tomllib.loads(str(string))
-
-    def __repr__(self):
-        return "%s(%r)" % (type(self).__name__, str(self))
-
-    def __setitem__(self, keys, value):
+    @setdocstring
+    def __setitem__(self, keys:Iterable, value:TOTALUNION):
         keys = _get_keys(keys)
         if not keys:
             self.data = value
@@ -96,19 +100,17 @@ class Holder(datahold.OkayDict):
         ans[keys[0]] = value
         self._data = data
 
+    @setdocstring
     def __str__(self) -> str:
         return tomli_w.dumps(self._data)
 
-    @functools.wraps(dict.clear)
-    def clear(self):
-        return self._data.clear()
-
     @property
-    def data(self):
+    @setdocstring
+    def data(self) -> Dict[str, Any]:
         return _copy_dict(self._data)
 
     @data.setter
-    def data(self, value):
+    def data(self, value:Any) -> None:
         self._data = _copy_dict(value)
 
     @data.deleter
@@ -116,28 +118,39 @@ class Holder(datahold.OkayDict):
         self.clear()
 
     def dump(self, file):
+        """dump into toml file"""
         with open(file, "w") as s:
             s.write(str(self))
 
-    @staticmethod
-    def fromdict(dictionary):
-        ans = Holder()
+    @classmethod
+    def fromdict(cls, dictionary:Any):
+        """from a dict"""
+        ans = cls()
         ans.data = dictionary
         return ans
+    @classmethod
+    def fromstr(cls, string:str):
+        """from a str"""
+        ans = cls()
+        ans._data = tomllib.loads(string)
+        return ans
 
-    def get(self, *keys, default=None):
+    @setdocstring
+    def get(self, *keys:KEYUNION, default:Any=None) -> TOTALUNION:
         try:
             return self[keys]
         except KeyError:
             return default
 
     @classmethod
-    def load(cls, file):
+    def load(cls, file:str) -> Self:
+        """load from toml file"""
         with open(file, "r") as s:
             text = s.readlines()
         return cls(text)
 
-    def setdefault(self, *keys, default):
+    @setdocstring
+    def setdefault(self, *keys, default:Any) -> Any:
         if not keys:
             return _copy_dict(self._data)
         keys = list(keys)
@@ -156,3 +169,4 @@ class Holder(datahold.OkayDict):
             ans[keys[0]] = _copy(default)
             self._data = data
             return ans[keys[0]]
+
