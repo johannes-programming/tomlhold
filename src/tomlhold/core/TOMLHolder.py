2fc26b43fcc4f0e476b61c64a65ecba4e4658954
Johannes <johannes.programming@gmail.com>
2026-06-13 18:15:13 +0200

    x

--- src/tomlhold/core/TOMLHolder.py
+++ src/tomlhold/core/TOMLHolder.py
@@ -1,87 +1,86 @@
-import io
-import operator
-import tomllib
-from collections.abc import Iterable, Mapping, Sequence
-from datetime import date, datetime, time
-from functools import partial
-from typing import Any, Self, cast, overload
+from __future__ import annotations
 
-import datahold  # type: ignore[import-untyped]
+import operator
+from collections.abc import Iterable, Iterator, Mapping, Sequence, Callable
+from typing import Any, Optional, Self, overload, ParamSpecArgs
+
 import setdoc
-import tomli_w
+import datahold
 from frozendict import frozendict
+from datetime import datetime, date, time
+from functools import partial, wraps
+import enum
 
 __all__ = ["TOMLHolder"]
 
 
-def getdict(
-    data: Mapping[str, Any],
-    /,
-    *,
-    freeze: bool = False,
-) -> dict[str, Any] | frozendict[str, Any]:
-    "This function returns a TOML dict."
-    ans: dict[str, Any]
-    x: str
-    z: str
-    ans = dict()
-    for x in frozendict(data).keys():
-        if isinstance(x, str):
-            z = str(x)
-            ans[z] = getvalue(data[z], freeze=freeze)
-            continue
-        raise TypeError(
-            f"The type {type(x).__name__!r} is not allowed for keys."
-        )
-    return frozendict(ans) if freeze else ans
+class Missing(enum.Enum):
+    missing = None
 
 
-@overload
-def getkey(key: int) -> int: ...
-
-
-@overload
-def getkey(key: str) -> str: ...
-
-
-def getkey(key: int | str) -> int | str:
-    "This function returns a TOML key."
-    if isinstance(key, int):
-        return operator.index(key)
-    if isinstance(key, str):
-        return str(key)
-    raise TypeError(
-        f"The type {type(key).__name__!r} is not allowed for keys."
-    )
-
-
-def getkeys(keys: int | str | tuple[int | str, ...], /) -> list[int | str]:
-    "This function returns TOML keys."
-    if isinstance(keys, int | str):
-        return [getkey(keys)]
+def find(data:dict[str, Any], key:Any, /) -> tuple[Any, Any]:
+    if isinstance(key, tuple):
+        keys = tuple(key)
     else:
-        return list(map(getkey, keys))  # type: ignore[arg-type]
+        keys = key,
+    ans = data
+    for x in keys[:-1]:
+        if isinstance(ans, dict):
+            ans = ans.setdefault(str(x), dict())
+        else:
+            ans = ans[x]
+    if isinstance(ans, dict):
+        return ans, str(keys[-1])
+    else:
+        return ans, keys[-1]
 
 
-def getvalue(value: Any, /, *, freeze: bool = False) -> Any:
+
+
+def getdict(mapping: Mapping, /, *, freeze:bool) -> dict:
+    ans:dict[str, Any]
+    x:object
+    y:Any
+    ans = dict()
+    for x in mapping.keys():
+        y=getvalue(mapping[x], freeze=freeze)
+        if y is not None:
+            ans[str(x)] = y
+    if freeze:
+        return frozendict(ans)
+    else:
+        return ans
+
+def getkey(key: object) -> int|str:
+    try:
+        return operator.index(key)
+    except Exception:
+        return str(key)
+    
+def getkeys(keys: object) -> tuple[int|str]:
+    if isinstance(keys, tuple):
+        return tuple(map(getkey, keys))
+    else:
+        return getkey(keys),
+
+def getvalue(value: Any, /, *, freeze: bool) -> Any:
     "This function returns a TOML value."
-    g: Iterable[Any]
+    g: Iterator
+    if value is None:
+        return None
     if isinstance(value, Mapping):
         return getdict(value, freeze=freeze)
     if isinstance(value, str):
         return str(value)
     if isinstance(value, Sequence):
         g = map(partial(getvalue, freeze=freeze), value)
+        g = filter(partial(operator.is_not, None), g)
         if freeze:
             return tuple(g)
         else:
             return list(g)
     if isinstance(value, bool):
         return bool(value)
-    if isinstance(value, float):
-        return float(value)
-    if isinstance(value, int):
-        return operator.index(value)
     if isinstance(value, datetime):
         return datetime(
             year=value.year,
@@ -100,6 +99,10 @@ def getvalue(value: Any, /, *, freeze: bool = False) -> Any:
             month=value.month,
             day=value.day,
         )
+    if isinstance(value, float):
+        return float(value)
+    if isinstance(value, int):
+        return operator.index(value)
     if isinstance(value, time):
         return time(
             hour=value.hour,
@@ -113,118 +116,158 @@ def getvalue(value: Any, /, *, freeze: bool = False) -> Any:
         f"The type {type(value).__name__!r} is not allowed for values."
     )
 
-
-class TOMLHolder(datahold.OkayDict):
-
-    @setdoc.basic
-    def __delitem__(
-        self: Self,
-        keys: int | str | tuple[int | str, ...],
-    ) -> None:
-        data_: dict | list
-        keys_: list[int | str]
-        lastkey: int | str
-        keys_ = getkeys(keys)
-        try:
-            lastkey = keys_.pop()
-        except Exception:
-            self.clear()
-            return
-        data_ = self._data  # type: ignore[assignment]
-        while keys_:
-            data_ = data_[keys_.pop(0)]  # type: ignore[index]
-        del data_[lastkey]  # type: ignore[arg-type]
+class TOMLHolder(datahold.HoldDict):
 
     @setdoc.basic
-    def __getitem__(
-        self: Self,
-        keys: int | str | tuple[int | str, ...],
-    ) -> Any:
-        data_: Any
-        keys_: list
-        key: Any
-        keys_ = getkeys(keys)
-        data_ = self._data
-        for key in keys_:
-            data_ = data_[key]
-        return getvalue(data_)
+    def __delitem__(self: Self, key: Any, /) -> None:
+        operator.delitem(*self._find(key))
 
+    @setdoc.basic
+    def __getitem__(self: Self, key: Any, /) -> Any:
+        return getvalue(operator.getitem(*self._find(key)), freeze=False)
+    
     @setdoc.basic
     def __setitem__(
         self: Self,
-        keys: int | str | tuple[int | str, ...],
+        key: object,
         value: Any,
+        /,
     ) -> None:
-        data: Any
-        k: Any
-        keys_: list[int | str]
-        lastkey: Any
-        target: Any
-        keys_ = getkeys(keys)
-        if keys_ == []:
-            self.data = value
+        if value is None:
+            try:
+                del self[key]
+            except Exception:
+                pass
             return
-        lastkey = keys_.pop()
-        data = getdict(self._data)
-        target = data
-        for k in keys_:
-            if isinstance(target, dict):
-                target = target.setdefault(k, {})
+        operator.setitem(*self._find(key), getvalue(value, freeze=False))
+    
+    def _find(self:Self, key:Any, /) -> tuple[Any, Any]:
+        if isinstance(key, tuple):
+            keys = tuple(key)
+        else:
+            keys = key,
+        ans = self._data
+        for x in keys[:-1]:
+            if isinstance(ans, dict):
+                ans = ans.setdefault(str(x), dict())
             else:
-                target = target[k]
-        target[lastkey] = value
-        self.data = data
-
+                ans = ans[x]
+        if isinstance(ans, dict):
+            return ans, str(keys[-1])
+        else:
+            return ans, keys[-1]
+        
+    
     @property
     @setdoc.basic
-    def data(self: Self) -> frozendict[str, Any]:
-        return cast(frozendict[str, Any], getdict(self._data, freeze=True))
-
+    def data(self:Self) -> frozendict[str, Any]:
+        return getvalue(self._data, freeze=True)
+    
     @data.setter
-    def data(self: Self, value: Mapping[str, Any], /) -> None:
-        self._data = cast(frozendict[str, Any], getdict(value))
+    def data(self:Self, value:Any) -> None:
+        if value is None:
+            del self.data
+            return
+        self._data = getdict(value, freeze=False)
 
-    def dump(self: Self, stream: Any, **kwargs: Any) -> None:
-        "This method dumps the data into a byte stream."
-        tomli_w.dump(self._data, stream, **kwargs)
-
-    def dumpintofile(self: Self, file: str, **kwargs: Any) -> None:
-        "This method dumps the data into a file."
-        with open(file, "wb") as stream:
-            self.dump(stream, **kwargs)
-
-    def dumps(self: Self, **kwargs: Any) -> str:
-        "This method dumps the data as a string."
-        return tomli_w.dumps(self._data, **kwargs)
-
-    def get(self: Self, *keys: int | str, default: Any = None) -> Any:
-        "This method returns self[*keys] if that exists, otherwise default."
-        try:
-            return self[keys]
-        except KeyError:
-            return default
-
-    @classmethod
-    def load(cls: type[Self], stream: Any, **kwargs: Any) -> Self:
-        "This classmethod loads data from byte stream."
-        return cls(tomllib.load(stream, **kwargs))
-
-    @classmethod
-    def loadfromfile(cls: type[Self], file: str, **kwargs: Any) -> Self:
-        "This classmethod loads data from file."
-        stream: io.BufferedReader
-        with open(file, "rb") as stream:
-            return cls.load(stream, **kwargs)
-
-    @classmethod
-    def loads(cls: type[Self], string: str, **kwargs: Any) -> Self:
-        "This classmethod loads data from string."
-        return cls(tomllib.loads(string, **kwargs))
+    @data.deleter
+    def data(self:Self) -> None:
+        self._data.clear()
 
     @setdoc.basic
-    def setdefault(self: Self, *keys: int | str, default: Any) -> Any:
+    def get(
+        self: Self,
+        key: Any,
+        default: Any = None,
+        /,
+        *,
+        frozen: bool = False,
+    ) -> Any:
         try:
-            return self[keys]
+            return getvalue(operator.getitem(*self._find(key)), freeze=frozen)
         except Exception:
-            self[keys] = default
-            return self[keys]
+            return default
+
+
+    @overload
+    @setdoc.setdoc(datahold.DataDict.pop.__doc__)
+    def pop(
+        self: Self, 
+        key: object, 
+        /, 
+        *, 
+        frozen:bool = False,
+    ) -> Any: ...
+
+    @overload
+    @setdoc.setdoc(datahold.DataDict.pop.__doc__)
+    def pop(
+        self: Self,
+        key: object,
+        default: Any,
+        /,
+        *, 
+        frozen:bool = False,
+    ) -> Any: ...
+
+    @setdoc.setdoc(datahold.DataDict.pop.__doc__)
+    def pop(
+        self: Self,
+        key: object,
+        default: Any = Missing.missing,
+        /,
+        *, 
+        frozen:bool = False,
+    ) -> Any:
+        args:tuple
+        data_, x = self._find(key)
+        if isinstance(default, Missing):
+            return data_.pop(x)
+        try:
+            return data_.pop(x)
+        except Key
+
+        keys = getkeys(key)
+        unfrozen=self._data
+        if not keys:
+            del self.data
+            return getdict(unfrozen, freeze=frozen)
+        if isinstance(default, Missing):
+            for x in keys[:-1]:
+                unfrozen = unfrozen[x]
+            return unfrozen.pop(keys[-1])
+        try:
+            for x in keys[:-1]:
+                unfrozen = unfrozen[x]
+            return unfrozen.pop(keys[-1])
+        except Exception:
+            return default
+
+    @setdoc.setdoc(dict.setdefault.__doc__)
+    def setdefault(
+        self: Self,
+        key: object,
+        default: Any = None,
+        /,
+    ) -> Optional[Value]:
+        ans: Optional[Value]
+        data: dict[Key | str, Optional[Value]]
+        data = dict(self.data)
+        ans = data.setdefault(key, default)
+        self.data = frozendict(data)
+        return ans
+
+    @setdoc.setdoc(dict.update.__doc__)
+    def update(
+        self: Self,
+        other: (
+            SupportsKeysAndGetitem[Key | str, Optional[Value]]
+            | Iterable[tuple[Key | str, Optional[Value]]]
+        ) = (),
+        /,
+        **kwargs: Optional[Value],
+    ) -> None:
+        data: dict[Key | str, Optional[Value]]
+        data = dict(self.data)
+        data.update(other, **kwargs)
+        self.data = frozendict(data)
